946a0065111ca362a72e7b0a1ba68fadffa836cf
roem9 <muhammadrum04@gmail.com>
2023-06-06 14:13:58 +0700

    ubah web

--- application/views/pages/sertifikat.php
+++ application/views/pages/sertifikat.php
@@ -36,8 +36,8 @@
                                                 Berlaku Sampai : <?= tgl_indo($tgl_berakhir)?>
                                             </p>
                                             <p><small class="text-danger"><i>Catatan : Data diatas adalah data tes TOEFL peserta yang sebenar-benarnya. Dan tidak ada pengurangan dan penambahan nilai sedikitpun</i></small></p>
-                                            <p><b>Masih belum puas dengan skor TOEFL yang Anda dapatkan hari ini? Yuk Belajar TOEFL Online di</b> <a href="https://merryenglish.id/" target="_blank"><b>merryenglish.id</b></a></p>
-                                            <p><b>Bahasa Inggris jadi mudah hanya di</b> <a href="https://merryenglish.id/" target="_blank"><b>merryenglish.id</b></a></p>
+                                            <p><b>Masih belum puas dengan skor TOEFL yang Anda dapatkan hari ini? Yuk Belajar TOEFL Online di</b> <a href="https://kampunginggrisceria.com/" target="_blank"><b>kampunginggrisceria.com</b></a></p>
+                                            <p><b>Bahasa Inggris jadi mudah hanya di</b> <a href="https://kampunginggrisceria.com/" target="_blank"><b>kampunginggrisceria.com</b></a></p>
                                         </div>
                                     </div>
                                 <?php else :?>
